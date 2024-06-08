23aac8a911d19f465a2f709863f37b8c403e1780
MrRobot44100 <ndoumathejoy@gmail.com>
2024-06-08 11:59:56 +0200

    modif .host Media query

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,25 +1,9 @@
-// Header.jsx
 import { Link, useLocation } from 'react-router-dom';
 import logo from '../../assets/logo.svg';
 import styles from '../Header/Header.module.css';
-import { useEffect, useState } from 'react';
 
 export default function Header() {
   const location = useLocation();
-  const [activeLink, setActiveLink] = useState('');
-
-  useEffect(() => {
-    // Set the active link based on the current path
-    if (location.pathname === '/') {
-      setActiveLink('Accueil');
-    } else if (location.pathname === '/about') {
-      setActiveLink('À propos');
-    }
-  }, [location.pathname]);
-
-  const handleLinkClick = (linkName) => {
-    setActiveLink(linkName);
-  };
 
   return (
     <header className={styles.header}>
@@ -29,15 +13,13 @@ export default function Header() {
       <nav className={styles.nav}>
         <Link
           to="/"
-          className={activeLink === 'Accueil' ? styles.activeLink : ''}
-          onClick={() => handleLinkClick('Accueil')}
+          className={location.pathname === '/' ? styles.activeLink : ''}
         >
           Accueil
         </Link>
         <Link
           to="/about"
-          className={activeLink === 'À propos' ? styles.activeLink : ''}
-          onClick={() => handleLinkClick('À propos')}
+          className={location.pathname === '/about' ? styles.activeLink : ''}
         >
           À propos
         </Link>
